3baa14de20f397c50ed4b7f520958b699b5fb7b0
Alan Costa Facchini <costafacchini@gmail.com>
2024-05-17 10:43:00 -0300

    Change js manager dependency from Webpack to esbuild

---
+++ app/javascript/application.js
@@ -0,0 +1,8 @@
+// Entry point for the build script in your package.json
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
